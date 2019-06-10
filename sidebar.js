eebabefbd5fc3a62f2e7c9ac3b1f6426b7f5f5d3
Vinaymeldrum <vmeld1@ocdsb.ca>
2019-06-10 19:35:40 -0400

    Changed style of navbar

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+function load(url, element) {
+  req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send(null);
+  element.innerHTML = req.responseText;
+}
+
+load("sidebar.html", document.getElementById("loadHeader"));
