ca21509b561362994775f281b7d940c9df99c9a4
Vinaymeldrum <vmeld1@ocdsb.ca>
2019-06-23 10:34:36 -0400

    Added js file for sidebar

--- sidebar.js
+++ sidebar.js
@@ -6,3 +6,26 @@ function load(url, element) {
 }
 
 load("sidebar.html", document.getElementById("loadHeader"));
+  $(document).ready(function() {
+  console.log("ready");
+
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function() {
+    console.log("click");
+    // open or close navbar
+    $('#sidebar').toggleClass('active');
+    if ($('#sidebar').hasClass('active')) {
+      $('#buttonDiv').css("padding-left", "250px");
+    } else {
+      $('#buttonDiv').css("padding-left", "0px");
+    }
+    // close dropdowns
+    $('.collapse.in').toggleClass('in');
+    // and also adjust aria-expanded attributes we use for the open/closed arrows
+    // in our CSS
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+});
